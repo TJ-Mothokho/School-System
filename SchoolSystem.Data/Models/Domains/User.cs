6dcefbfe6fd071af856502bb270f8cf5fc48642b
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-10-24 07:45:40 +0200

    Modified email property to company email

--- SchoolSystem.Data/Models/Domains/User.cs
+++ SchoolSystem.Data/Models/Domains/User.cs
@@ -8,7 +8,13 @@ namespace School_System.Data.Models.Domains
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
+        public string Email 
+        {
+            get
+            {
+                return $"{FirstName.ToLower()}{LastName.ToLower()}{Id}@academy.ac.za";
+            }
+        }
         public string Password { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public string Gender { get; set; }
