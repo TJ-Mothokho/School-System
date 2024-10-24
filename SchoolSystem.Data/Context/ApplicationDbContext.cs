9461627361341f937b6dab47c45f7844583ddab1
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-10-24 06:41:03 +0200

    Added DbContext

---
+++ SchoolSystem.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using School_System.Data.Models.Domains;
+
+namespace School_System.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+
+
+    }
+}
