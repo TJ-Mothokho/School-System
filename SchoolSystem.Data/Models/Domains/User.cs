b9028ecd60e2011fd6b9d42c7964af325f6eeac0
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-10-24 06:43:48 +0200

    Added User Model

---
+++ SchoolSystem.Data/Models/Domains/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School_System.Data.Models.Domains
+{
+    public abstract class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public string Status { get; set; }
+    }
+    public enum Role
+    {
+        
+    }
+}
